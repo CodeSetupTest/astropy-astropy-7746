ac25bbf044d7a3f06a33b77777625f6682052fd1
Ubuntu <sasha@anysphere.co>
2025-06-26 23:58:03 +0000

    Initial commit for SWE-bench instance astropy__astropy-7746

---
+++ setup.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euxo pipefail
+
+# SWE-bench instance setup script (git operations removed)
+# The repository code is already present in this directory
+
+#!/bin/bash
+set -euxo pipefail
+chmod -R 777 /testbed
+cd /testbed
+source /opt/miniconda3/bin/activate
+conda activate testbed
+echo "Current environment: $CONDA_DEFAULT_ENV"
+python -m pip install -e .[test] --verbose
+git config --global user.email setup@swebench.config
+git config --global user.name SWE-bench
+git commit --allow-empty -am SWE-bench
